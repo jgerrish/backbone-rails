646b934143762f7b0fba6e4f061756838f25f886
Chris Kite <chris@chriskite.com>
2013-07-22 14:23:36 -0500

    fix handling of checkboxes in backbone_datalink.js

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -5,16 +5,34 @@
         var el, name;
         el = $(this);
         name = el.attr("name");
-        model.bind("change:" + name, function() {
-          return el.val(model.get(name));
-        });
-        return $(this).bind("change", function() {
-          var attrs;
-          el = $(this);
-          attrs = {};
-          attrs[el.attr("name")] = el.val();
-          return model.set(attrs);
-        });
+
+        if(typeof name !== "undefined" && name !== null) {
+          model.bind("change:" + name, function() {
+            var newVal = model.get(name);
+
+            if(el.is(":checkbox")) {
+              el.prop(checked: !!newVal);
+            } else {
+              return el.val(newVal);
+            }
+
+          });
+
+          return $(this).bind("change", function() {
+            var attrs;
+            el = $(this);
+            attrs = {};
+
+            if(el.is(":checkbox")) {
+              attrs[el.attr("name")] = el.prop("checked");
+            } else {
+              attrs[el.attr("name")] = el.val();
+            }
+
+            return model.set(attrs);
+          });
+        }
+
       });
     }
   });
